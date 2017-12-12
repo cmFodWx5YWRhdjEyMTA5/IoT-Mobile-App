a5aa0871714585a5fb821d7af9769dcb7912116f
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2017-12-12 21:50:42 +0300

    Connecting Wifi on ESP8266 module
    connected wifi

---
+++ Arduino/ThingSpeak.ino
@@ -0,0 +1,26 @@
+#include <ESP8266WiFi.h>
+#define WIFI_SSID "Laks.js"
+#define WIFI_PASSWORD "vakumvakumvakum"
+WiFiClient client;
+
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(9600);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+ 
+  }
+ 
+}
