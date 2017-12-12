d832b25d4086d08c4e0c931340ee1edec425927d
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2017-12-12 21:53:40 +0300

    insert data to cloud platform
    insert data to cloud platform

--- Arduino/ThingSpeak.ino
+++ Arduino/ThingSpeak.ino
@@ -1,9 +1,12 @@
 #include <ESP8266WiFi.h>
 #define WIFI_SSID "Laks.js"
 #define WIFI_PASSWORD "vakumvakumvakum"
-WiFiClient client;
+String apiKey = "YB0636AMO077W410";
+const char* server = "api.thingspeak.com";
 const int prob= A0;
 int measurement_Total;
+int duration=5;
+WiFiClient client;
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,7 +25,31 @@ void setup() {
 }
 
 void loop() {
- measurement_Total = analogRead(prob);
+   measurement_Total = analogRead(prob);
+   if (client.connect(server, 80)) {
+    String postStr = apiKey;
+    postStr += "&field1=";
+    postStr += String(measurement_Total);
+    postStr += "\r\n\r\n";
+   
+    //Uplad the postSting with temperature and Humidity information every
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
+     
+    Serial.print("Soil wetness: ");
+    Serial.println(measurement_Total);
+   
   }
- 
-  }
+  client.stop();
+
+  Serial.println("Waiting…");
+  // thingspeak needs minimum 15 sec delay between updates
+  delay(duration*1000);
+}
